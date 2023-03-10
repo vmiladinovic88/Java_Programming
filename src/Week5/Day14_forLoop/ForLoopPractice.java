2c2e5018c0f821cd1b8d073c8bde16b0a337ec31
vmiladinovic <veci_m@hotmail.com>
2023-03-10 11:47:44 -0500

    Initial commit

--- src/Week5/Day14_forLoop/ForLoopPractice.java
+++ src/Week5/Day14_forLoop/ForLoopPractice.java
@@ -6,5 +6,21 @@ public class ForLoopPractice {
             System.out.println("Hello Cydeo");
         }
         System.out.println("Hello World");
+        int sum = 0;
+        for(int i = 1; i<=100; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+
+        for(char i = 'A'; i <= 'Z'; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Hello");
+    for(char i = 'Z'; i>='A'; i--){
+        System.out.print(i + " ");
+    }
+        System.out.println();
+        System.out.println("Cydeo");
     }
 }
