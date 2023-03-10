2ab7b1206234e853aa3b67f75be369591e5250da
vmiladinovic <veci_m@hotmail.com>
2023-03-10 11:37:14 -0500

    Initial commit

---
+++ src/Week5/Day14_forLoop/ForLoopIntro.java
@@ -0,0 +1,9 @@
+package Week5.Day14_forLoop;
+
+public class ForLoopIntro {
+    public static void main(String[] args) {
+        for(int n=1;n <=10; n++){
+            System.out.println("Hello World");
+        }
+    }
+}
---
+++ src/Week5/Day14_forLoop/ForLoopPractice.java
@@ -0,0 +1,10 @@
+package Week5.Day14_forLoop;
+
+public class ForLoopPractice {
+    public static void main(String[] args) {
+        for(int i = 10; i>=2; i--){
+            System.out.println("Hello Cydeo");
+        }
+        System.out.println("Hello World");
+    }
+}
